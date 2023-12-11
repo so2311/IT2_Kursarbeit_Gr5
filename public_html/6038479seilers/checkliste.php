6ccba6797fdfcfc4aed964093de344fbb13a9b72
so2311 <145649675+so2311@users.noreply.github.com>
2023-12-11 21:22:09 +0100

    Add files via upload

---
+++ public_html/6038479seilers/checkliste.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	 <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deine Checklisten</title>
+<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+	<h1 style="color: black;">Deine Checklisten</h1>
+	<p></p>
+	<p></p>
+		<div class="container1">
+            <label for="listname">Modul:</label>
+            <input type="text" name="listName" required>
+            <br>
+	    	<br>
+            <label for="category">Kategorie:</label>
+            <input type="text" name="category" required>
+            <br>
+		    <br>
+            <label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+			<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		    <label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+			<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+		<br>
+		<br>
+	    	<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+			<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		    <label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+			<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+	</div>
+		
+			<div class="container2">
+		<label for="listname">Name der Checkliste:</label>
+            <input type="text" name="listName" required>
+		<br>
+		<br>
+            <label for="category">Kategorie:</label>
+            <input type="text" name="category" required>
+            <br>
+		    <br>
+            <label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+				<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+				<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+		<br>
+		<br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+				<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+				<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+			   <input type="date" name="dueDate" required>
+	      </div>
+          
+	<div class="container3">
+		<label for="listname">Name der Checkliste:</label>
+            <input type="text" name="listName" required>
+		<br>
+		<br>
+            <label for="category">Kategorie:</label>
+            <input type="text" name="category" required>
+            <br>
+		    <br>
+            <label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+            <br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+            <br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+		<br>
+		<br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+            <br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+            <input type="date" name="dueDate" required>
+            <br>
+	        <br>
+		<label for="task">Aufgabe:</label>
+            <input type="text" name="task" required>
+		<br>
+            <label for="dueDate">Fälligkeitsdatum:</label>
+			<input type="date" name="dueDate" required>
+    </div>
+	<br>
+	<br>
+
+<?php
+include 'fortschrittsbalken.html';
+?>
+	<button type="submit">Speichern</button>
+</body>
+</html>
