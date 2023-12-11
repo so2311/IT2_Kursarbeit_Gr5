4b784fb930d282cae3e1a99590827dcd3f0a71e7
allegradueser <146952424+allegradueser@users.noreply.github.com>
2023-12-11 21:40:23 +0100

    Add files via upload

--- public_html/6040337adueser/notizmanager.php
+++ public_html/6040337adueser/notizmanager.php
@@ -2,11 +2,23 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Unbenanntes Dokument</title>
+<title>Notizenmanager</title>
 </head>
+		<!-- Bootstrap Einbindung -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		
+	<!-- Bootstrap Einbindung CSS, Javascript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+	
+	<!-- responsive Design mit bootstrap -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+	
 
 <body>
 	<h1> Notizenmanager </h1>
+	<p>Mit diesem Notizmanager behältst du den Überblick über alle deine Module und deine Lernaktivitäten:</p>
+	<p>
 	
 </body>
 </html>
