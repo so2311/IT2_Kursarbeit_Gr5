b76f3c71c3a1f9d38f286b3f706162932f1cea26
allegradueser <146952424+allegradueser@users.noreply.github.com>
2023-12-11 21:29:09 +0100

    update

---
+++ public_html/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Internettechnologie 2</title>
+  </head>
+  <body>
+    <h1>Herzlich Willkommen im WS 2024</h1>
+	  
+	  <a href="seilers/" >Hier geht's zu Sophie</a>
+	  <a href="sschraeder/" >Hier geht's zu Selina</a>
+	  <a href="adueser/" >Hier geht's zu Allegra</a>
+	  
+  </body>
+</html>
