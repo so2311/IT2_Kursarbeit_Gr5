01ac240efca5ecd17f55c2f89d3cb8295114998b
Selina411 <145649718+Selina411@users.noreply.github.com>
2023-12-16 13:32:05 +0100

    Modulbeschreibungen, DB

---
+++ public_html/6039985sschraeder/php/verarbeitung.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>verarbeitung.php</title>
+</head>
+
+<body>
+	
+	<?php
+// Verbindung zur Datenbank herstellen
+include 'dbconnect.php'
+
+// Daten aus dem Formular holen
+$selectedKategorie = $_POST["kategorie"];
+$selectedModul = $_POST["modul"];
+$selectedNote = $_POST["note"];
+
+// SQL-INSERT-Befehl ausführen
+$sql = "INSERT INTO Note (note) VALUES ('$selectedNote')";
+$sql = "INSERT INTO Kategorie (kategorie) VALUES ('$selectedKategorie')";
+$sql = "INSERT INTO Modul (modul) VALUES ($selectedModul')";
+	
+// Verbindung zur Datenbank schließen
+$conn->close();
+?>
+	
+	
+</body>
+</html>
