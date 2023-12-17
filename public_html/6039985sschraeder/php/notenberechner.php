c6ef309bd099c11fcb0897db4983a8f7d103a349
Selina411 <145649718+Selina411@users.noreply.github.com>
2023-12-17 20:00:04 +0100

    update

--- public_html/6039985sschraeder/php/notenberechner.php
+++ public_html/6039985sschraeder/php/notenberechner.php
@@ -19,9 +19,6 @@
 	
 	<!-- Bootstrap Einbindung -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		
-	<!-- Bootstrap Einbindung CSS, Javascript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 	
 	<!-- responsive Design mit bootstrap -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -124,15 +121,12 @@
 	if(isset($_POST["ausrechnen"]))
 		
     //Berechnung
-		  $note = [hier irgendwie einzelnen Noten rein kriegen];
 
    $average =  array_sum($numbers)/count($numbers);
    print_r($average);
 		{
 						echo $average;
 					}
-	
-	
 		?>
 		
 		
