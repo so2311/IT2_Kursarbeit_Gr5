8e30fc94948a1f90c3ad82be4ce1055a3eed0b74
allegradueser <146952424+allegradueser@users.noreply.github.com>
2023-12-18 23:18:06 +0100

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -1,44 +1,31 @@
-<<<<<<< HEAD
 <!doctype html>
 <html>
   <head>
     <meta charset="UTF-8">
     <title>Startseite</title>
   </head>
-  <body>
+  <body class="Startseite">
+	  <header class=header>
+		  <h1>Startseite</h1>
+	  </header>
+	  <nav class=nav>
+		  <?php
+		  // Navigationsleiste
+		  	include "navigation.php";
+		  ?>
+		  </nav>
+		  
     <h1>Der Studierendenhelfer</h1>
 	  
-	  <a href="notenberechner.php" >Hier geht's zum Notenberechner</a>
+	  <a href="6039985sschraeder/php/notenberechner.php" >Hier geht's zum Notenberechner</a>
 	  <br>
 	  <br>
-	  <a href="checkliste.php" >Hier geht's zu deinen Checklisten</a>
+	  <a href="6038479seilers/checkliste.php" >Hier geht's zu deinen Checklisten</a>
 	  <br>
 	  <br>
-	  <a href="notizmanager.php" >Hier geht's zum Notizenmanager</a>
+	  <a href="6040337adueser/notizmanager.php" >Hier geht's zum Notizenmanager</a>
 	  <br>
 	  <br>
 	  <a href="ergebnisse.php" >Übersicht aller Ergebnisse</a>
   </body>
-=======
-<!doctype html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Startseite</title>
-  </head>
-  <body>
-    <h1>Der Studierendenhelfer</h1>
-	  
-	  <a href="notenberechner.php" >Hier geht's zum Notenberechner</a>
-	  <br>
-	  <br>
-	  <a href="checkliste.php" >Hier geht's zu deinen Checklisten</a>
-	  <br>
-	  <br>
-	  <a href="notizmanager.php" >Hier geht's zum Notizenmanager</a>
-	  <br>
-	  <br>
-	  <a href="ergebnisse.php" >Übersicht aller Ergebnisse</a>
-  </body>
->>>>>>> 4867574ebb1c093413f7eff94f2bbbb47c5a6f82
 </html>
