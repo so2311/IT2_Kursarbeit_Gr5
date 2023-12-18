fd9b1b4cb816b24a934ce5622e0eb6980479c592
allegradueser <146952424+allegradueser@users.noreply.github.com>
2023-12-18 23:08:34 +0100

    Add files via upload

--- public_html/6040337adueser/notizmanager.php
+++ public_html/6040337adueser/notizmanager.php
@@ -1,3 +1,7 @@
+<?php
+ error_reporting(E_ALL);
+ ini_set("display_errors", 1);
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,16 +12,50 @@
     <title> Notizenmanager</title>
 </head>
 	
+<?php include "../db_connect.php"; 
+	
+	if (!empty($_POST)) {
+   $sql = "INSERT INTO Notizen (Semester, Kategorie, Kurs, Notizen) VALUES ('".$_POST["semester"]."','".$_POST["kategorie"]."', '".$_POST["kurs"]."', '".$_POST["notiz"]."')";
+
+$db_erg = mysqli_query( $can, $sql );
+if ( ! $db_erg )
+{
+  die('Ungültige Abfrage: ' . mysqli_error());
+}  
+} else {  
+    
+}
+	
+	
+$sql = "SELECT * FROM Semester";
+
+$db_erg = mysqli_query( $can, $sql );
+if ( ! $db_erg )
+{
+  die('Ungültige Abfrage: ' . mysqli_error());
+}
+var_dump($_POST);
+	echo '<table border="1">';
+while ($zeile = mysqli_fetch_array( $db_erg, MYSQLI_ASSOC))
+{
+  echo "<tr>";
+  echo "<td>". $zeile['SemesterID'] . "</td>";
+  echo "<td>". $zeile['Semester'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+?>	
 <body>
 	
 	<h1> Notizenmanager </h1>
 	<p>Mit diesem Notizenmanager behältst du den Überblick über alle deine Module und deine Lernaktivitäten:</p>
 <!-- Semester kann ausgewählt werden -->
 <!-- 4. Semester entfällt, da es sich um das Praxissemester handelt -->
+	<form action="notizmanager.php" method="post">
 <div class="form-container">
     <div class="left-column">
         <label for="semester">Semester:</label>
-        <select id="semester" onchange="updateCourses()">
+        <select name="semester" id="semester" onchange="updateCourses()">
             <option value="">Bitte wählen</option>
             <option value="1">Semester 1</option>
             <option value="2">Semester 2</option>
@@ -31,7 +69,7 @@
 
         <!-- Kategorie kann ausgewählt werden -->
         <label for="category">Kategorie:</label>
-        <select id="category" onchange="updateCourses()" disabled>
+        <select name="kategorie" id="category" onchange="updateCourses()" disabled>
             <option value="" selected disabled>Bitte wählen</option>
             <option value="gestaltung">Gestaltung</option>
             <option value="technik">Technik</option>
@@ -43,7 +81,7 @@
 
         <!-- Kurse werden dynamisch mit JavaScript hinzugefügt -->
         <label for="course">Kurs:</label>
-        <select id="course" onchange="updateCourses()" disabled>
+        <select name="kurs" id="course" onchange="updateCourses()" disabled>
             <option value="" selected disabled>Bitte wählen</option>
         </select>
         <br>
@@ -55,11 +93,11 @@
 		 <div id="notes-container">
 			 <!-- class und style einfügen -->
         <label for="notes">Notizen:</label>
-        <textarea id="notes"></textarea>
-        <button class="button" onclick="saveNotes()">Notizen speichern</button>
+        <textarea id="notes" name="notiz"></textarea>
+        <button type="submit" class="button" >Notizen speichern</button>
     </div>
 </div>
-		
+	</form>		
 </body>
 <script src="notizmanager.js"></script>
 </html>
