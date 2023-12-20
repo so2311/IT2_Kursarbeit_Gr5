2c6d17cd83a9252b52f7ae759ce900699ca8a575
allegradueser <146952424+allegradueser@users.noreply.github.com>
2023-12-21 00:03:12 +0100

    Add files via upload

--- public_html/ergebnisse.php
+++ public_html/ergebnisse.php
@@ -1,3 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+?>
 <!doctype html>
 <html>
 <head>
@@ -6,21 +10,57 @@
 </head>
 
 	<!-- stylesheet verlinkung-->
-	<link rel="stylesheet" href="stylesheet.css">
-
+	<link rel="stylesheet" href="../public_html/6040337adueser/css/style.css">
+<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 	<div class="container">
 
-<body class="Startseite">
-	  <header class=header>
-		  <h1>Startseite</h1>
 	  </header>
-	  <nav class=nav>
-		  <?php
-		  // Navigationsleiste
-		  	include "navigation.php";
-		  ?>
-		  </nav>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Studierendenhelfer</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item"><a class="nav-link" href="index.php">Startseite</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="6039985sschraeder/php/notenberechner.php" id="notenDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Notenberechner
+                </a>
+                <div class="dropdown-menu" aria-labelledby="notenDropdown">
+                    <a class="dropdown-item" href="6039985sschraeder/php/notenberechner.php">Notenrechner</a>
+                    <a class="dropdown-item" href="6039985sschraeder/php/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="6039985sschraeder/php/modul.php">Modul</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="checklisteDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Checkliste
+                </a>
+                <div class="dropdown-menu" aria-labelledby="checklisteDropdown">
+                    <a class="dropdown-item" href="6038479seilers/checkliste.php">Checkliste</a>
+                    <a class="dropdown-item" href="6038479seilers/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="6038479seilers/modul.php">Modul</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="notizmanagerDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Notizmanager
+                </a>
+                <div class="dropdown-menu" aria-labelledby="notizmanagerDropdown">
+                    <a class="dropdown-item" href="6040337adueser/notizmanager.php">Manager</a>
+                    <a class="dropdown-item" href="6040337adueser/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="6040337adueser/semester.php">Semester</a>
+                </div>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="ergebnisse.php">Ergebnisse</a></li>
+        </ul>
+    </div>
+</nav>
+</body>	
 
 <body>
 
@@ -31,13 +71,6 @@
 <h1>Übersicht der Ergebnisse </h1>
 </header>
 	
-<nav>
-	Navigation
-	</nav>
-	
-<side>
-	Link zurück?
-	</side>
 
 	<main>
 <h2>Deine Ergebnisse aus dem Notenberechner</h2>
@@ -47,14 +80,17 @@
 
     ?>
 	
-	
+<br>	
 	
 <h2>Deine Ergebnisse aus den Checklisten</h2>
 <p> Anzahl der Checklisten</p>
+<?php
+        include "6038479seilers/checklistergebnisse.php";
+
+        ?>
 
 
 
-<br>
 <br>
 	
 	
@@ -73,7 +109,9 @@
 	
 </main>
 	
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
-
+<footer style="color:white">Jade Hochschule; Der Studierendenhelfer von Selina Schräder, Allegra Düser und Sophie Eilers</footer>
 </html>
