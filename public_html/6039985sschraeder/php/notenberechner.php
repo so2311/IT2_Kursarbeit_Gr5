de24b9ec35ecfe1893f89ad10e9effcb07d351db
Selina411 <145649718+Selina411@users.noreply.github.com>
2023-12-21 00:12:43 +0100

    Add files via upload

--- public_html/6039985sschraeder/php/notenberechner.php
+++ public_html/6039985sschraeder/php/notenberechner.php
@@ -59,7 +59,7 @@ $db_erg_kategorie = mysqli_query($con, $sql);
 </head>
 <body>
 	<!---Navigationsmenü--->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Studierendenhelfer</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -67,38 +67,38 @@ $db_erg_kategorie = mysqli_query($con, $sql);
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item"><a class="nav-link" href="index.php">Startseite</a></li>
+            <li class="nav-item"><a class="nav-link" href="../index.php">Startseite</a></li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="6039985sschraeder/php/notenberechner.php" id="notenDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Notenberechner
+                <a class="nav-link dropdown-toggle" href="../6039985sschraeder/php/notenberechner.php" id="notenDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					  Notenberechner
                 </a>
                 <div class="dropdown-menu" aria-labelledby="notenDropdown">
-                    <a class="dropdown-item" href="6039985sschraeder/php/notenberechner.php">Notenrechner</a>
-                    <a class="dropdown-item" href="6039985sschraeder/php/kategorie.php">Kategorie</a>
-                    <a class="dropdown-item" href="6039985sschraeder/php/modul.php">Modul</a>
+                    <a class="dropdown-item" href="../6039985sschraeder/php/notenberechner.php">Notenrechner</a>
+                    <a class="dropdown-item" href="../6039985sschraeder/php/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6039985sschraeder/php/modul.php">Modul</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="checklisteDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="../6038479seilers/checkliste.php" id="checklisteDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Checkliste
                 </a>
                 <div class="dropdown-menu" aria-labelledby="checklisteDropdown">
-                    <a class="dropdown-item" href="6038479seilers/checkliste.php">Checkliste</a>
-                    <a class="dropdown-item" href="6038479seilers/kategorie.php">Kategorie</a>
-                    <a class="dropdown-item" href="6038479seilers/modul.php">Modul</a>
+                    <a class="dropdown-item" href="../6038479seilers/checkliste.php">Checkliste</a>
+                    <a class="dropdown-item" href="../6038479seilers/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6038479seilers/modul.php">Modul</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="notizmanagerDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="../6040337adueser/notizmanager.php" id="notizmanagerDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Notizmanager
                 </a>
                 <div class="dropdown-menu" aria-labelledby="notizmanagerDropdown">
-                    <a class="dropdown-item" href="6040337adueser/notizmanager.php">Manager</a>
-                    <a class="dropdown-item" href="6040337adueser/kategorie.php">Kategorie</a>
-                    <a class="dropdown-item" href="6040337adueser/semester.php">Semester</a>
+                    <a class="dropdown-item" href="../6040337adueser/notizmanager.php">Manager</a>
+                    <a class="dropdown-item" href="../6040337adueser/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6040337adueser/semester.php">Semester</a>
                 </div>
             </li>
-            <li class="nav-item"><a class="nav-link" href="ergebnisse.php">Ergebnisse</a></li>
+            <li class="nav-item"><a class="nav-link" href="../ergebnisse.php">Ergebnisse</a></li>
         </ul>
     </div>
 </nav>
