467176d1fc0b5c44f6ccdc5108b3ac6981ea6bc8
so2311 <145649675+so2311@users.noreply.github.com>
2023-12-20 22:18:36 +0100

    update

--- public_html/6038479seilers/checkliste.php
+++ public_html/6038479seilers/checkliste.php
@@ -12,24 +12,27 @@ ini_set("display_errors", 1);
 
     <title>Deine Checklisten</title>
     <link rel="stylesheet" href="css/checkliste.css">
-    <script src="checkliste.js"></script>
+    
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<header class=header>
-    <h1>Checklisten</h1>
-    <img src="../bilder/Checklisten%20header.png" alt="Header">
-</header>
+
+
 <?php
 $can= mysqli_connect("m12242-08.kurs.jade-hs.de", "m12242-08", "cwAH6n59E","m12242-08_2");
 
 if (!empty($_POST)) {
     var_dump($_POST);
-    $sql = "INSERT INTO Aufgaben (Module, Kategorien, Aufgaben, Datum) VALUES ('" . $_POST["modul"] . "','" . $_POST["kategorie"] . "', '" . $_POST["task"] . "', '" . $_POST["datum"] . "')";
+    $sql = "INSERT INTO Aufgaben (Module, Kategorien, Aufgaben, Datum) VALUES ('" . $_POST["Module"] . "','" . $_POST["Kategorien"] . "', '" . $_POST["Aufgaben"] . "', '" . $_POST["Datum"] . "')";
 
     $db_erg = mysqli_query($can, $sql);
     if (!$db_erg) {
         die('Ungültige Abfrage: ' . mysqli_error());
     }
-} else {
+} 
+	else {
+	
+	$sql = "SELECT * FROM Aufgaben JOIN Kategorien ON Aufgaben.Kategorien = Kategorien.KategorieID JOIN Module ON Aufgaben.Module = Module.ModulID ORDER BY AufgabeID DESC LIMIT 10 ";
 
 }
 
@@ -41,20 +44,65 @@ $sql = "SELECT * FROM Module";
 
 $db_erg_modul = mysqli_query($can, $sql);
 ?>
-
+<main>
 <body>
-<h1 style="color: black;">Deine Checklisten</h1>
-<p></p>
-<p></p>
+	 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Studierendenhelfer</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item"><a class="nav-link" href="../index.php">Startseite</a></li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="../6039985sschraeder/php/notenberechner.php" id="notenDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					  Notenberechner
+                </a>
+                <div class="dropdown-menu" aria-labelledby="notenDropdown">
+                    <a class="dropdown-item" href="../6039985sschraeder/php/notenberechner.php">Notenrechner</a>
+                    <a class="dropdown-item" href="../6039985sschraeder/php/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6039985sschraeder/php/modul.php">Modul</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="../6038479seilers/checkliste.php" id="checklisteDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Checkliste
+                </a>
+                <div class="dropdown-menu" aria-labelledby="checklisteDropdown">
+                    <a class="dropdown-item" href="../6038479seilers/checkliste.php">Checkliste</a>
+                    <a class="dropdown-item" href="../6038479seilers/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6038479seilers/modul.php">Modul</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="../6040337adueser/notizmanager.php" id="notizmanagerDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Notizmanager
+                </a>
+                <div class="dropdown-menu" aria-labelledby="notizmanagerDropdown">
+                    <a class="dropdown-item" href="../6040337adueser/notizmanager.php">Manager</a>
+                    <a class="dropdown-item" href="../6040337adueser/kategorie.php">Kategorie</a>
+                    <a class="dropdown-item" href="../6040337adueser/semester.php">Semester</a>
+                </div>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="../ergebnisse.php">Ergebnisse</a></li>
+        </ul>
+    </div>
+</nav>
+	<header>
+		<h1 style="color: black;">Checkliste</h1>
+	<img src="Checklisten header.png" alt="Header">
+</header>
+	<br>
+	<br>
 <form action="checkliste.php" method="post">
 
-    <!-- Class=conatainer wurde verwendet, um die drei Checklisten zu ordnen und angemessen darstellen zu können -->
+    <!-- Class=conatainer wurde verwendet, um den Auswahl-Block als ganzes formatieren zu können -->
 
     <div class="container1">
         <!-- Der Name der Checkliste wird eingegeben, bzw. das Modul, für das man eine Checkliste anlegen möchte -->
-        <label for="listname">Modul:</label>
-        <select name="modul" required>
+        <label style="color: white;" for="Module">Modul:</label>
+        <select name="Module" required>
             <option value="">Bitte Wählen</option>
             <?php
             while ($zeile = mysqli_fetch_array($db_erg_modul, MYSQLI_ASSOC)) {
@@ -65,8 +113,8 @@ $db_erg_modul = mysqli_query($can, $sql);
         <br>
         <br>
         <!-- Die Kategorie wird eingegeben, bzw. die vier Säulen des Studiengangs -->
-        <label for="kategorie">Kategorie:</label>
-        <select name="kategorie" required>
+        <label style="color: white;" for="Kategorien">Kategorie:</label>
+        <select name="Kategorien" required>
             <option value="">Bitte Wählen</option>
             <?php
             while ($zeile = mysqli_fetch_array($db_erg_kategorie, MYSQLI_ASSOC)) {
@@ -78,13 +126,13 @@ $db_erg_modul = mysqli_query($can, $sql);
         <br>
         <!-- Insgesamt können pro Checkliste vier fällige Aufgaben eingegeben werden und das zugehörige Fälligkeitsdatum wird ausgewählt -->
 
-        <label for="task">Aufgabe:</label>
-        <input type="text" name="task" required>
+        <label style="color: white;" for="Aufgaben">Aufgabe:</label>
+        <input type="text" name="Aufgaben" required>
         <br>
-        <label for="dueDate">Fälligkeitsdatum:</label>
-        <input type="date" name="datum" required>
-        <br>
-        <br>
+		<br>
+        <label style="color: white;" for="Datum">Fälligkeitsdatum:</label>
+        <input type="date" name="Datum" required>
+        
     </div>
     <br>
     <br>
@@ -92,13 +140,8 @@ $db_erg_modul = mysqli_query($can, $sql);
     <!-- Mit dem Button werden die Daten gespeichert -->
 
     <button type="submit" onclick="addTask ()">Aufgabe hinzufügen</button>
+	</main>
     </form>
     <br>
-
-    <?php
-
-    include 'fortschrittsbalken.php';
-
-    ?>
 </body>
 </html>
