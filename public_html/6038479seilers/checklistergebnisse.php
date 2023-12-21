808c3827f5e415ed6d927bd3da7eb4f55332b907
so2311 <145649675+so2311@users.noreply.github.com>
2023-12-21 01:18:51 +0100

    update

--- public_html/6038479seilers/checklistergebnisse.php
+++ public_html/6038479seilers/checklistergebnisse.php
@@ -18,157 +18,20 @@ if ($num > 0) {
     echo "Keine Ergebnisse <br>";
 }
 
-// Summe der Aufgaben mit dem Fälligkeitsdatum 19. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-19";
-$resultSum = mysqli_query($con, $sqlSum);
+$sqlDistinctDates = "SELECT DISTINCT Datum FROM Aufgaben";
+$resultDistinctDates = mysqli_query($con, $sqlDistinctDates);
 
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 19. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
+while ($dateRow = mysqli_fetch_array($resultDistinctDates, MYSQLI_ASSOC)) {
+    $currentDate = $dateRow['Datum'];
 
-// Summe der Aufgaben mit dem Fälligkeitsdatum 20. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-20";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 20. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 21. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-21";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 21. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 22. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-22";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 22. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 23. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-23";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 23. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 24. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-24";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 24. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 25. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-25";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 25. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 26. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-26";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 26. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 27. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-27";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 27. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 28. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-28";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 28. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 29. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-29";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 29. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 30. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-30";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 30. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-// Summe der Aufgaben mit dem Fälligkeitsdatum 31. Januar 2024
-$sqlSum = "SELECT COUNT(Datum) AS sum_value FROM Aufgaben WHERE Datum = 2024-01-31";
-$resultSum = mysqli_query($con, $sqlSum);
-
-$rowSum = mysqli_fetch_assoc($resultSum);
-if ($rowSum['sum_value'] !== "0") {
-    echo "Anzahl der Aufgaben mit dem Fälligkeitsdatum 31. Januar 2024: " . $rowSum['sum_value'] . "<br>";
-} else {
-    echo "Keine Ergebnisse <br>";
-}
-
-$sqlCount = "SELECT * FROM Aufgaben";
-$resultSelect = mysqli_query($con, $sqlCount);
-
-while ($zeile = mysqli_fetch_array($resultSelect, MYSQLI_ASSOC)) {
-    $sqlCount = "SELECT COUNT(Datum) AS count_value FROM Aufgaben WHERE Aufgaben = ".$zeile['Datum'];
+    $sqlCount = "SELECT COUNT(*) AS count_value FROM Aufgaben WHERE Datum = '$currentDate'";
     $resultCount = mysqli_query($con, $sqlCount);
+
     $rowCount = mysqli_fetch_assoc($resultCount);
-    echo $zeile['Datum']." : " . $rowCount['count_value'] . "<br>";
+    echo $currentDate . " : " . $rowCount['count_value'] . "<br>";
 }
+
+// Schließe die Verbindung zur Datenbank
+mysqli_close($con);
 
 ?>
